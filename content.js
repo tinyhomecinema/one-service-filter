34a20abc092beef85688d742407bf8be4efa2a94
Cineminha <tinyhomecinema@gmail.com>
2025-03-20 08:17:15 -0300

    Added code to not execute script if user is not logged in

--- content.js
+++ content.js
@@ -1,24 +1,33 @@
 (function () {
   "use strict";
 
+  // If user is not logged in, do not load menu item
+  if (!document.body.getAttributeNode('class').value.includes("logged-in")) {return}
+
   var films;
+  var numberOfFilms;
 
   // Get Services menu
   if (window.location.href.includes("/watchlist/")) {
-    films = document.getElementsByClassName('poster-list -p125 -grid -constrained')[0];
+      films = document.getElementsByClassName('poster-list -p125 -grid -constrained')[0];
+      if (!films) {
+        films = document.getElementsByClassName('poster-list -p125 -grid -scaled128')[0];
+      }
   } else {
     films = document.getElementsByClassName('js-list-entries poster-list -p125 -grid film-list')[0];
   }
 
+  numberOfFilms = films.childNodes.length;
+
   // Remove nodes which are not film nodes
-  for (let i = films.childNodes.length-1; i >= 0; i--) {
-    let node = films.childNodes[i];
+  for (let i = numberOfFilms-1; i >= 0; i--) {
+    var node = films.childNodes[i];
     if (!node.attributes) {
       films.removeChild(node);
     }
   }
 
-  var numberOfFilms = films.childNodes.length;
+  numberOfFilms = films.childNodes.length;
 
   // Create Services menu item to remove films
   if (numberOfFilms > 0) {
@@ -61,35 +70,36 @@
       for (let i = 0; i < servicesUrls.length; i++) {
         if (servicesUrls[i]) {
           removeFilms(servicesUrls[i], i);
-          // Run a second time due to some films not
-          // being removed in the first run in some cases
-          removeFilms(servicesUrls[i], i);
         }
       }
-
     }
 
     async function removeFilms(url, index) {
 
       // Get page content for other services
-      let pageObject = await fetch(url);
-      let pageText = await pageObject.text();
-      let serviceName = servicesList[index];
+      var pageObject = await fetch(url);
+      var pageText = await pageObject.text();
+      var serviceName = servicesList[index];
+
+      var node ;
+      var filmId;
+      var filmName;
 
       // Check if film is in another service and remove it
-      for (let i = 0; i < films.childNodes.length; i++) {
+      for (var i = films.childNodes.length-1; i >= 0; i--) {
         try {
-          let node = films.childNodes[i];
-          let filmId = node.getElementsByTagName('div')[0].getAttributeNode('data-item-id').value;
-          let filmName = node.getElementsByTagName('div')[0].getAttributeNode('data-film-name').value;
-
+          node = films.childNodes[i];
+          filmId = node.getElementsByTagName('div')[0].getAttributeNode('data-item-id').value;
           if (pageText.search(filmId) != -1) {
             films.removeChild(node);
+            try {
+              filmName = node.getElementsByTagName('div')[0].getAttributeNode('data-film-name').value;
+            } catch {filmName = filmId}
             console.log("'" + filmName + "' removed. Available also on '" + serviceName + "'");
             numberOfFilms--;
           }
         } catch(err) {
-          console.log("ERROR: Unable to remove film.");
+          console.log("ERROR: Unable to remove '" + filmName + "'.");
           console.log("ERROR: " + err);
         }
       }
